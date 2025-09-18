609f3559b383b78024e44f3e0758aef8a43647be
Ayo <Ayoroq@gmail.com>
2025-09-17 22:46:10 -0400

    Added a test image that can be dragged

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import "./style.css";
 import { Ship, Gameboard, Player } from "./game";
 import { createGrid, clearGrid } from "./grid";
 
-
 clearGrid();
 createGrid();
+
+function handleDragStart(e) {
+  this.style.opacity = "0.4";
+}
+
+function handleDragEnd(e) {
+  this.style.opacity = "1";
+}
+
+let items = document.querySelectorAll(".container .box");
+items.forEach(function (item) {
+  item.addEventListener("dragstart", handleDragStart);
+  item.addEventListener("dragend", handleDragEnd);
+});
