07ffbccc18f8e4e9a0c89e136bc30e1dea4256d1
Ayo <Ayoroq@gmail.com>
2025-09-17 23:40:40 -0400

    Deleting past code to restart the process

--- src/index.js
+++ src/index.js
@@ -6,16 +6,3 @@ import { createGrid, clearGrid } from "./grid";
 clearGrid();
 createGrid();
 
-function handleDragStart(e) {
-  this.style.opacity = "0.4";
-}
-
-function handleDragEnd(e) {
-  this.style.opacity = "1";
-}
-
-let items = document.querySelectorAll(".container .box");
-items.forEach(function (item) {
-  item.addEventListener("dragstart", handleDragStart);
-  item.addEventListener("dragend", handleDragEnd);
-});
