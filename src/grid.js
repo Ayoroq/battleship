8a7b08f677e1da75e05d11a3589648b3a5906f77
Ayo <Ayoroq@gmail.com>
2025-09-23 21:19:14 -0400

    Fix undefined variable error in renderShips function
    Also, restructuring the renderShips function to add ships to first cell

--- src/grid.js
+++ src/grid.js
@@ -1,8 +1,5 @@
 import { Gameboard } from "./game.js";
 
-// const gameboard = new Gameboard();
-// const grid = document.querySelector(".grid-container-1");
-
 function clearGrid(gridSelector) {
   gridSelector.innerHTML = "";
 }
@@ -29,10 +26,12 @@ function renderBoard(gameboard, gameboardContainer) {
 
 function renderShips(gameboard, gameboardContainer) {
   // Clear existing ship elements
-  gameboardContainer.querySelectorAll('.ship-element').forEach(ship => ship.remove());
-  
+  gameboardContainer
+    .querySelectorAll(".ship-element")
+    .forEach((ship) => ship.remove());
+
   const processedShips = new Set();
-  
+
   for (let x = 0; x < gameboard.boardSize; x++) {
     for (let y = 0; y < gameboard.boardSize; y++) {
       const ship = gameboard.board[x][y];
@@ -40,43 +39,22 @@ function renderShips(gameboard, gameboardContainer) {
         processedShips.add(ship);
         
         // Find ship direction and start position
-        let direction = 'horizontal';
+        let direction = "horizontal";
         let startX = x, startY = y;
-        
+
         // Check if ship extends vertically
         if (x + 1 < gameboard.boardSize && gameboard.board[x + 1][y] === ship) {
-          direction = 'vertical';
+          direction = "vertical";
         }
-        
-        // Create ship element
-        const shipElement = document.createElement('div');
-        shipElement.className = 'ship-element';
-        //shipElement.classList.add('ship');
-        shipElement.draggable = true;
-        shipElement.dataset.shipName = ship.name;
-        shipElement.dataset.shipSize = ship.length;
-        shipElement.dataset.shipDirection = direction;
-        shipElement.textContent = `${ship.name}`;
-        
-        // Position the ship absolutely
-        const containerWidth = gameboardContainer.clientWidth;
-        const containerHeight = gameboardContainer.clientHeight;
-        const cellWidth = containerWidth / gameboard.boardSize;
-        const cellHeight = containerHeight / gameboard.boardSize;
-        
-        shipElement.style.position = 'absolute';
-        shipElement.style.left = `${startY * cellWidth + 1}px`;
-        shipElement.style.top = `${startX * cellHeight + 1}px`;
-        
-        if (direction === 'horizontal') {
-          shipElement.style.width = `${ship.length * cellWidth - 2}px`;
-          shipElement.style.height = `${cellHeight - 2}px`;
-        } else {
-          shipElement.style.width = `${cellWidth - 2}px`;
-          shipElement.style.height = `${ship.length * cellHeight - 2}px`;
+
+        // Get the cell with the starting position
+        const startCell = gameboardContainer.querySelector(
+          `[data-x="${startX}"][data-y="${startY}"]`
+        );
+
+        if (startCell) {
+          startCell.classList.add("ship-element");
         }
-        
-        gameboardContainer.appendChild(shipElement);
       }
     }
   }
