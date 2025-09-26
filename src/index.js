b0106d0c71a6f167ee3f0bbcb29988d4e7667587
Ayo <Ayoroq@gmail.com>
2025-09-25 20:00:49 -0400

    Restructuring the ship movement and grid rendering

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import "./reset.css";
 import "./style.css";
 import { Ship, Gameboard, Player } from "./game.js";
-import { renderBoard } from "./grid.js";
-import { shipPlacement} from "./shipPlacement.js";
-
-const gameboard = new Gameboard();
-const grid = document.querySelector(".grid-container-1");
-
-renderBoard(gameboard, grid);
-// renderShips(gameboard, grid);
-shipPlacement(gameboard, grid);
--- src/ship-movement.js
+++ src/ship-movement.js
@@ -1,11 +1,2 @@
 // This is the module that handles the movement of ships, the drag and drops and the repositioning
 
-function handleDragStart(e) {
-  this.style.opacity = "0.4";
-}
-
-function handleDragEnd(e) {
-  this.style.opacity = "1";
-}
-
-export { handleDragStart, handleDragEnd };
