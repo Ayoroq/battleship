da73a26ac315af22a7fc9dcb41bc947cbcf32e34
Ayo <Ayoroq@gmail.com>
2025-10-01 19:18:43 -0400

    feat: implement loading and ship placement screen transitions with fade-in effect

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import "./reset.css";
 import "./style.css";
 import { initializeGame } from "./game-controller.js";
 import { Ship, Gameboard, Player } from "./game.js";
+
+const loadingScreen = document.querySelector(".loading-screen");
+const shipPlacementScreen = document.querySelector(".ship-placement-screen");
+
+function gameFlow(){
+  // Show loading screen initially 
+  setTimeout(() => {
+    loadingScreen.style.display = "none";
+    shipPlacementScreen.style.display = "flex";
+  }, 5000);
+}
+
+gameFlow();
